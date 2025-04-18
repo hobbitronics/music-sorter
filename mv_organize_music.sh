0136c3b46ed15189ba685ef0046493ab26053132
Michael Wilson <miguel.dw@gmail.com>
2025-04-19 06:46:03 +0930

    deduplicate file types

--- mv_organize_music.sh
+++ mv_organize_music.sh
@@ -81,7 +81,7 @@ done
 
 find "$SOURCE" "$DEST" -type d -empty -delete
 echo "Moved $MOVED out of $TOTAL files. $NOT_AUDIO files were not audio.\n"
-echo "File types: "${FILE_TYPES[@]}\n""
+echo "File types: "$(printf "%s\n" "${FILE_TYPES[@]}" | sort -u)"\n"
 if [ "${#FAILURE_LOG[@]}" -ne 0 ]; then
     printf '%s\n' "${FAILURE_LOG[@]}" | sort | uniq -c
 fi
